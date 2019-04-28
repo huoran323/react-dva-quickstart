6653e6f162234ac2f69af78d665286ba1e9b8f2e
霍然 <303176152@qq.com>
2019-04-28 16:58:00 +0800

    third submit

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -2,7 +2,6 @@ import React from "react";
 import { connect } from "dva";
 import ProductList from "./../components/ProductList";
 
-//定义函数组件
 const Products = ({ dispatch, products }) => {
   function handleDelete(id) {
     dispatch({
@@ -19,6 +18,15 @@ const Products = ({ dispatch, products }) => {
 };
 
 // export default Products;
+// connect 方法返回的也是一个 React 组件，通常称为容器组件
+//因为它是原始 UI 组件的容器，即在外面包了一层 State。
+// 第一个参数，传入一个products,返回一个经过action处理的新的products,更新了state(下面的写法一样)
 export default connect(({ products }) => ({
   products
 }))(Products);
+
+// export default connect(({ products }) => {
+//   return {
+//     products: products
+//   };
+// })(Products);
